93723ca77e4fc229219fa6342005db2eb5a327df
hendi <hendi@xulu.co.id>
2021-07-04 21:14:03 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/login/provider/login_provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.login.provider.login_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:login_provider/app/app.dart';
+import 'package:flutter/material.dart' show runApp;
+
+void main() {
+  runApp(App());
+}
