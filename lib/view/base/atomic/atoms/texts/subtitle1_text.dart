768e6be70e5a5eba7ee1e6b7b04ebdbdca8f0226
hendi <hendi@xulu.co.id>
2021-07-20 12:21:40 +0700

    add texts atoms

---
+++ lib/view/base/atomic/atoms/texts/subtitle1_text.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Subtitle1Text extends StatelessWidget {
+  final String data;
+  final Color color;
+  const Subtitle1Text({Key? key, required this.data, required this.color}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Text(
+      data,
+      style: theme.textTheme.subtitle1!.copyWith(color: color),
+    );
+  }
+}
